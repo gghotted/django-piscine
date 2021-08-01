a27a7c4361e9828a4f5c1157081feadccbf19435
gghotted <gghotted2@naver.com>
2021-08-01 22:46:35 +0900

    Update ex03

--- day04/ex03/d04/ex03/views.py
+++ day04/ex03/d04/ex03/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'ex03/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['colors_title'] = ['black', 'red', 'blue', 'green']
+
+        increase = range(0, 250, 5)
+        max = [245] * 50
+        black = zip(increase, increase, increase)
+        red = zip(max, increase, increase)
+        blue = zip(increase, increase, max)
+        green = zip(increase, max, increase)
+        colors_rows = zip(black, red, blue, green)
+        context['colors_rows'] = colors_rows
+        return context
