27a2585c33570e42189a8eb832b949d0e31c458d
gghotted <gghotted2@naver.com>
2021-08-01 12:41:54 +0900

    Update when end of url is slash

--- day04/ex01/d04/ex01/urls.py
+++ day04/ex01/d04/ex01/urls.py
@@ -3,7 +3,7 @@ from ex01.views import DjangoView, DisplayView, TemplatesView
 
 app_name = 'ex01'
 urlpatterns = [
-    path('django', DjangoView.as_view(), name='django'),
-    path('display', DisplayView.as_view(), name='display'),
-    path('templates', TemplatesView.as_view(), name='templates')
+    path('django/', DjangoView.as_view(), name='django'),
+    path('display/', DisplayView.as_view(), name='display'),
+    path('templates/', TemplatesView.as_view(), name='templates')
 ]
