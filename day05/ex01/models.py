ef4267831f2ab17e19a3535e3ddb08539ad6650b
gghotted <gghotted2@naver.com>
2021-08-04 15:38:38 +0900

    Add unique

--- day05/ex01/models.py
+++ day05/ex01/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Movies(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=64, unique=True)
     episode_nb = models.IntegerField(primary_key=True)
     opening_crawl = models.TextField(null=True)
     director = models.CharField(max_length=32)
